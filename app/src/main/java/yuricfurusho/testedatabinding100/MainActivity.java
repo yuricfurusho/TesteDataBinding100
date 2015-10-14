b43a39a24151ba4717f041b59f01ae384307b496
yuricfurusho <yuricfurusho@livetouch.com.br>
2015-10-14 15:36:53 -0300

    ajustes

--- app/src/main/java/yuricfurusho/testedatabinding100/MainActivity.java
+++ app/src/main/java/yuricfurusho/testedatabinding100/MainActivity.java
@@ -2,6 +2,7 @@ package yuricfurusho.testedatabinding100;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView firstName = (TextView) findViewById(R.id.tFirstName);
+        firstName.setText("Yuri");
+
+        TextView lastName = (TextView) findViewById(R.id.tLastName);
+        lastName.setText("Furusho");
+
     }
 }
---
+++ app/src/main/java/yuricfurusho/testedatabinding100/User.java
@@ -0,0 +1,14 @@
+package yuricfurusho.testedatabinding100;
+
+/**
+ * Created by live on 14/10/15.
+ */
+public class User {
+    public String firstName;
+    public String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
